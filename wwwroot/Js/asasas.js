0c2ca7ab50ee4f2fe96dd60875550b3a282b75da
Gharamahmed <136538126+foxcode111@users.noreply.github.com>
2023-09-28 20:10:55 +0300

    chathub

---
+++ wwwroot/Js/asasas.js
@@ -0,0 +1,17 @@
+﻿function onFileSelected() {
+    let profile = "";
+    const inputNode: any = document.getElementById('ProfileName');
+
+    if (typeof FileReader !== 'undefined') {
+        const reader = new FileReader();
+
+        reader.onload = (e: any) => {
+            profile = e.target.result;
+
+            document
+                .getElementById('ProfileImg')
+                ?.setAttribute('src', this.profile);
+        };
+        reader.readAsDataURL(inputNode.files[0]);
+    }
+}
